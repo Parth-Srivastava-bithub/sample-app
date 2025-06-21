38dd6eae5007ee079c8bb93a5e17245535ca1e39
Parth Srivastava <parthsrivastava6112004@gmail.com>
2025-06-21 20:20:14 +0530

    react update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+let name = "Parth Srivastava";
+let a = 5;
+let b = 7;
+const variable = true;
+let externalHTML = "<b>This is bold</b>";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <p>This is {name} and this is sum {a + b} and this is external HTML {externalHTML} which can't be parsed in HTML for security purpose</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, magnam accusamus! Nulla velit a inventore magni porro id eligendi commodi molestiae, placeat totam corrupti aut eaque, natus aperiam reiciendis dolores?</p>
+      </div>
+      <input type="text" className='input-box' class="inputClass" />
+      <div>
+        <h1 class="welcome">{variable && "Hello there!"}</h1>
+      </div>
+    </>
+
   );
 }
 
